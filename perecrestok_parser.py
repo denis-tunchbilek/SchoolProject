eae58a578039dc8526d6c6e0d37c436b9c041e85
denis-tunchbilek <86554612+denis-tunchbilek@users.noreply.github.com>
2023-04-05 15:46:12 +0300

    Update perecrestok_parser.py

--- perecrestok_parser.py
+++ perecrestok_parser.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
-import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from selenium.webdriver.common.by import By
+
 
 
 
@@ -9,10 +12,10 @@ class perik_pars(object):
   def __init__(self, product):
     self.product = product
     self.link = "https://www.perekrestok.ru/cat/search?search=" + self.product
-    self.driver = webdriver.Chrome()
+    self.driver = webdriver.Chrome("./chromedriver")
   def pars(self):
       self.driver.get(self.link)
-      time.sleep(2)
+      WebDriverWait(self.driver, timeout=10).until(presence_of_element_located((By.XPATH, ".//div[@class=\'product-card-wrapper\']")))
       self.page_source = self.driver.page_source
       self.soup = bs(self.page_source, "lxml")
       for i in self.soup.find_all("div", class_="sc-hzMMVR eSdfDu"):
@@ -22,5 +25,5 @@ class perik_pars(object):
         print(name, price, rating)
 
 
-muka = perik_pars("каньяк")
+muka = perik_pars("мука")
 muka.pars()
